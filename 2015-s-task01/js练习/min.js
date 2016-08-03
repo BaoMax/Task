295ee0186f1e9df1fb78006c8f24cfa5a0548308
weiru <weiru@rd.tuan800.com>
2016-08-03 19:09:19 +0800

    []属性选择器的实现

--- 2015-s-task01/js练习/min.js
+++ 2015-s-task01/js练习/min.js
@@ -1,24 +1,55 @@
+function getAttrElement(node,attr,value){
+	if(node.nodeName == "#document"){
+		node = node.children[0];
+	}
+	if(node.getAttribute(attr) === value){
+		return node;
+	}
+	for(var i = 0,l = node.childElementCount;i < l;i += 1){
+		var item = node.children[0];
+		var result = getAttrElement(item,attr,value);
+		if(result){
+			return result;
+		}
+	}
+	return false;
+}
+function hasAttrElement(node,attr){
+	if(node.nodeName == "#document"){
+		node = node.children[0];
+	}
+	if(node.getAttribute(attr)){
+		return node;
+	}
+	for(var i = 0,l = node.childElementCount;i < l;i += 1){
+		var item = node.children[0];
+		var result = getAttrElement(item,attr);
+		if(result){
+			return result;
+		}
+	}
+	return false;
+}
 function $(selector){
 	var list = selector.split(/\s+/);
 	var node = document;
 	for(var i = 0,l = list.length;i < l;i += 1){
-		var item = list[0];
+		var item = list[i];
 		var sign = item.charAt(0);
 		if((/^#\w+$/).test(item)){
 			node = node.getElementById(item.substring(1));
-		}else if((/^.\w+/).test(item)){
+		}else if((/^\.\w+/).test(item)){
 			node = node.getElementsByClassName(item.substring(1))[0];
-		}else if((/^\[\w+=\w+\]$/).test(item)){
-			var value = item.split("=")[1];
-			while(node.childElementCount){
-				for(var i = 0,l = node.childElementCount;i < l;i += 1){
-					var item = node.children[i];
-				}
-			}
-		}else if((/^\[\w+\]$/).test(item)){
-
-		}else if((/^w+$/).test(item)){
+		}else if((/^\[.+=.+\]$/).test(item)){
+			var value = item.substring(1,item.length - 1).split("=")[1];
+			var attr = item.substring(1,item.length - 1).split("=")[0];
+			node = getAttrElement(node,attr,value);
+		}else if((/^\[.+\]$/).test(item)){
+			var attr = item.substring(1,item.length - 1);
+			node = getAttrElement(node,attr);
+		}else if((/^\w+$/).test(item)){
 			node = node.getElementsByTagName(item)[0];
 		}
 	}
+	return node;
 }
